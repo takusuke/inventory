3114996e440f91a74c045e179c09a5e8e86426ab
takusuke <massive@mac.com>
2023-05-07 20:15:29 +0900

    add some files

---
+++ public/frontend.js
@@ -0,0 +1,55 @@
+const API_URL = "/api/beans";
+
+async function getInventory() {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+}
+
+async function loadInventory() {
+  const inventory = await getInventory();
+  const inventoryList = document.getElementById("inventory-list");
+  inventoryList.innerHTML = "";
+
+  inventory.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
+    listItem.textContent = `${item.variety} - 入庫日: ${item.entryDate} - 価格: ${item.price} - 使用量: ${item.usedAmount} - 残り: ${item.stockAmount} - 前月からの繰越量: ${item.previousMonthCarryOverAmount} - その月最も使用されている豆: ${item.totalUsedAmount}`;
+    inventoryList.appendChild(listItem);
+  });
+}
+
+async function addBean(variety, entryDate, price, usedAmount, stockAmount, previousMonthCarryOverAmount, totalUsedAmount) {
+  await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ variety, entryDate, price, usedAmount, stockAmount, previousMonthCarryOverAmount, totalUsedAmount }),
+  });
+
+  loadInventory();
+}
+
+const inventoryList = document.getElementById("inventory-list");
+if (inventoryList) {
+  loadInventory();
+}
+
+const addBeanForm = document.getElementById("add-bean-form");
+if (addBeanForm) {
+  addBeanForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const variety = document.getElementById("variety").value;
+    const entryDate = document.getElementById("entry-date").value;
+    const price = document.getElementById("price").value;
+    const usedAmount = document.getElementById("used-amount").value;
+    const stockAmount = document.getElementById("stock-amount").value;
+    const previousMonthCarryOverAmount = document.getElementById("previous-month-carry-over-amount").value;
+    const totalUsedAmount = document.getElementById("total-used-amount").value;
+
+    addBean(variety, entryDate, price, usedAmount, stockAmount, previousMonthCarryOverAmount, totalUsedAmount);
+
+    // フォームをリセットする
+    event.target.reset();
+  });
+}
--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+
 const app = express();
-const port = 3000;
+const db = new sqlite3.Database(':memory:');
 
-const CoffeeBeanInventory = require('./inventory');
-const inventory = new CoffeeBeanInventory();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+db.serialize(() => {
+  db.run('CREATE TABLE inventory (id INTEGER PRIMARY KEY, variety TEXT, entry_date TEXT, price REAL, used_amount REAL, remaining_amount REAL, previous_month_carry_over_amount REAL, total_used_amount REAL, stock_amount REAL)');
+
+  db.run('INSERT INTO inventory (variety, entry_date, price, used_amount, remaining_amount, previous_month_carry_over_amount, total_used_amount, stock_amount) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', ['コロンビア', '2023-01-01', 1000, 50, 150, 200, 100, 100]);
+});
+
 app.get('/api/inventory', (req, res) => {
-  inventory.getAllBeans((beans) => {
-    res.json(beans);
+  db.all('SELECT * FROM inventory', (err, rows) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(rows);
+    }
   });
 });
 
 app.post('/api/inventory', (req, res) => {
-  const { variety, entryDate, price, usedAmount, remainingAmount, previousMonthCarryOverAmount, totalUsedAmount } = req.body;
-  inventory.addBeans(variety, entryDate, parseFloat(price), parseInt(usedAmount, 10), parseInt(remainingAmount, 10), parseInt(previousMonthCarryOverAmount, 10), parseInt(totalUsedAmount, 10));
-  res.sendStatus(200);
+  const { variety, entryDate, price, usedAmount, remainingAmount, previousMonthCarryOverAmount, totalUsedAmount, stockAmount } = req.body;
+
+  db.run('INSERT INTO inventory (variety, entry_date, price, used_amount, remaining_amount, previous_month_carry_over_amount, total_used_amount, stock_amount) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [variety, entryDate, price, usedAmount, remainingAmount, previousMonthCarryOverAmount, totalUsedAmount, stockAmount], function (err) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.sendStatus(201);
+    }
+  });
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Inventory app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
